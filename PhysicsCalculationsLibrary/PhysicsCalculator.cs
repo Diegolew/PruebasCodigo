7b8f21e826a203848ef5546fb524789747f99133
Diegolew <diegolewensztain2@gmail.com>
2024-05-28 19:04:19 -0400

    Creacion del proyecto y creacion de la primera funcion

---
+++ PhysicsCalculationsLibrary/PhysicsCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PhysicsCalculationsLibrary
+{
+    public class PhysicsCalculator
+    {
+        public double CalculateForce(double mass, double acceleration)
+        {
+            return mass * acceleration;
+        }
+
+    }
+}
+
+
